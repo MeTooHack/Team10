4194e2efffa9f3e241196993730b1c290b747932
Jacob Waller <jaco@spotify.com>
2018-02-03 14:11:02 +0100

    Add initial `/gender` analyse call

--- frontend/src/AudioRecorder.js
+++ frontend/src/AudioRecorder.js
@@ -7,13 +7,14 @@ export default class AudioRecorder extends React.Component {
 
     this.state = {
       isRecording: false,
-      blobUrl: null
+      blob: null
     };
 
     this.start = this.start.bind(this);
     this.stop = this.stop.bind(this);
     this.resume = this.resume.bind(this);
     this.pause = this.pause.bind(this);
+    this.analyse = this.analyse.bind(this);
   }
 
   start() {
@@ -25,7 +26,8 @@ export default class AudioRecorder extends React.Component {
         this.mediaRecorder = new MediaStreamRecorder(stream);
         this.mediaRecorder.stream = stream;
         this.mediaRecorder.mimeType = "audio/wav"; // check this line for audio/wav
-        this.mediaRecorder.start(3000);
+        this.mediaRecorder.start();
+        this.mediaRecorder.ondataavailable = blob => this.setState({ blob })
         this.setState({ isRecording: true });
       },
       console.error
@@ -45,11 +47,38 @@ export default class AudioRecorder extends React.Component {
     this.mediaRecorder.pause();
   }
 
+  async analyse() {
+    const formData = new FormData()
+    formData.append('audio', this.state.blob);
+
+    const response = await fetch('http://localhost:5000/gender', {
+      method: 'POST',
+      body: formData
+    });
+
+    if (!response.ok) {
+      this.setState({
+        error: {
+          status: response.statusCode,
+          statusText: response.statusText,
+          message: response.text()
+        }
+      });
+    } else {
+      this.setState({
+        error: null,
+        gender: await response.json()
+      })
+    }
+  }
+
   render() {
-    const { isRecording, blobURL } = this.state;
+    const { isRecording, blob, gender, error } = this.state;
     return (
       <div>
         <h2>Record Audio</h2>
+        <p>{JSON.stringify(gender)}</p>
+        <p>{JSON.stringify(error)}</p>
         <button onClick={this.start}>Start</button>
         <button onClick={this.stop} disabled={!isRecording}>
           Stop
@@ -60,7 +89,9 @@ export default class AudioRecorder extends React.Component {
         <button onClick={this.pause} disabled={!isRecording}>
           Pause
         </button>
-        <a href={blobURL}>Save</a>
+        <button onClick={this.analyse} disabled={!blob}>
+          Analyse
+        </button>
       </div>
     );
   }
