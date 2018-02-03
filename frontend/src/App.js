d420b7c8f70ed1dacf1ef19ed47b5b7c07eee31d
Erik Larkö <erik@mullvad.net>
2018-02-03 11:46:09 +0100

    Add suppression techniques list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { VoiceGraph } from "./VoiceGraph";
+import { SuppressionTechniquesList } from "./SuppressionTechniquesList";
 import "./App.css";
 
 export default function App() {
@@ -9,7 +10,15 @@ export default function App() {
         <h1 className="App-title">Team TÄN</h1>
       </header>
 
-      <VoiceGraph />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ flexGrow: 1 }}>
+          <VoiceGraph />
+        </div>
+
+        <div style={{ width: '400px' }}>
+          <SuppressionTechniquesList />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/SuppressionTechniquesList.js
@@ -0,0 +1,57 @@
+// @flow
+
+import React from "react";
+import "./SuppressionTechniquesList.css";
+
+const techniques = [
+  {
+    name: "Making Invisible",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+  },
+  {
+    name: "Ridicule",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+  },
+  {
+    name: "Withholding information",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+  },
+  {
+    name: "Double bind",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+  },
+  {
+    name: "Heap blame or Put to shame",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+  },
+  {
+    name: "Objectifying ",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+  },
+  {
+    name: "Force or Threat to force",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+  },
+];
+export function SuppressionTechniquesList() {
+  return (
+    <div className="suppression-list">
+      {techniques.map(t => <Technique key={t.name} technique={t} />)}
+    </div>
+  );
+}
+
+function Technique(props) {
+    const { technique } = props;
+    return <div className="suppression-technique">
+        <h1 className="suppression-name">{technique.name}</h1>
+        <div className="suppression-desc">{technique.description}</div>
+    </div>
+}
