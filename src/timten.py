2cf08ea65a3fcc828329b24db57e5101fbcc7567
Jessica Andersson <jessica.andersson@meltwater.com>
2018-02-03 12:03:55 +0100

    return json format

--- src/timten.py
+++ src/timten.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/gender')
+def gender():
+    result = {
+        "male" : 0.51,
+        "female" : 0.49
+    }
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
